f6dab9412087adb9f691ca670e58d72432035821
Benjamin Thomas <benjamin@thomasnetwork.net>
2019-04-03 15:30:27 -0400

    Documentation

--- ClipGrouper.py
+++ ClipGrouper.py
@@ -1,37 +1,66 @@
+"""
+Author:         Benjamin Thomas
+Major:          Software Development
+Creation Date:  April 5th, 2019
+Due Date:       April 5th, 2019
+Course:         Independent Study
+Professor:      Dr. Frye
+Filename:       CliperGrouper.py
+Purpose:        The program will look in the videofile directory for mp4 or mov files,
+                then use mediainfo to lookup and store the information about the video
+                files. Once sorted, the program will measure the gaps between the videos
+                and display an estimate for how the video clips should be grouped.
+"""
+
 import os
 import datetime
+import argparse
 from videoInfo import VideoInfo
+
+parser = argparse.ArgumentParser(description="Groups related video files in a directory based off the gaps inbetween.")
+parser.add_argument("directory", help="directory with video files")
+parser.parse_args()
+args = parser.parse_args()
+
+directory = args.directory
+
+if not directory.endswith("/"):
+    directory = directory + "/"
 
 videoFiles = []
 gaps = []
 gapsum = datetime.timedelta()
 
-for f in os.listdir('videofiles'):
-    if f.endswith('.MP4'):
-        file = VideoInfo('videofiles/' + f)
-        print file.filename
+for f in os.listdir(directory):
+    if f.lower().endswith('.mp4') or f.lower().endswith('.mov'):
+        file = VideoInfo(directory + f)
         videoFiles.append(file)
 
-print len(videoFiles)
-
+# sort the files by their start value
 sortedFiles = sorted(videoFiles, key=lambda vf: vf.start)
 
-print "should print all files except last one"
+for file in sortedFiles:
+    print file.filename + "," + str(file.start.strftime("%H:%M:%S")) + "," + str(file.duration)
+
 
 # itterate with i over sortedFiles except for last file
 for i, file1 in enumerate(sortedFiles[:-1]):
     file2 = sortedFiles[i + 1]
     gap = file2.start - file1.end
     gapsum += gap
-    print gap
+    #print file1
+    print "Video Gap between " + str(i) +  " and " + str(i + 1) + ": " + str(gap)
+
     gaps.append(gap)
 
 gapavg = gapsum / len(gaps)
 
-print "New group for " + sortedFiles[0].filename
+print "Avg Gap: %s\n" % str(gapavg)
+
+print "New group:\n\t" + sortedFiles[0].filename
 for i, gap in enumerate(gaps):
     nextClip = sortedFiles[i + 1]
     if gap < gapavg:
         print "\t" + nextClip.filename
     else:
-        print "New group for " + nextClip.filename
+        print "New group:\n\t" + nextClip.filename
--- videoInfo.py
+++ videoInfo.py
@@ -1,3 +1,17 @@
+"""
+Author:         Benjamin Thomas
+Major:          Software Development
+Creation Date:  April 5th, 2019
+Due Date:       April 5th, 2019
+Course:         Independent Study
+Professor:      Dr. Frye
+Filename:       videoInfo.py
+Purpose:        Class file that will read a video file and create an object with
+                the video's filename, start time, end time, and clip duration.
+"""
+
+import os
+import platform
 import pymediainfo
 import datetime
 import re
@@ -11,8 +25,12 @@ class VideoInfo:
         Converts a string in a datetime
     """
     def startToDateTime(self, c):
-        dt = datetime.datetime.strptime(c, "UTC %Y-%m-%d %H:%M:%S")
+        dt = datetime.datetime.fromtimestamp(c)
         return dt
+
+    def creationTime(self, p):
+        stat = os.stat(p)
+        return stat.st_mtime
 
     """
     function durationToTimeDelta(c)
@@ -31,17 +49,30 @@ class VideoInfo:
         t = datetime.timedelta(0, seconds, 0, milliseconds, minutes, hours)
         return t
 
+    """
+    function __init__(self, filename)
+    params:
+        filename - string: the video file to read
+    Desciption:
+        Reads the file and stores the information
+    """
     def __init__(self, filename):
         fileData = pymediainfo.MediaInfo.parse(filename)
         videoData = fileData.tracks[1]
 
-        self.startstring = videoData.to_data()["encoded_date"]
+        self.startstring = self.creationTime(filename)
         self.durationstring = videoData.to_data()["other_duration"][3]
 
         self.filename = filename
-        self.start = self.startToDateTime(str(self.startstring))
+        self.start = self.startToDateTime(self.startstring)
         self.duration = self.durationToTimeDelta(self.durationstring)
         self.end = self.start + self.duration
 
+    """
+    function __str__(self)
+    Desciption:
+        Overrides string operator and displays the video information
+    """
+
     def __str__(self):
-        return "File start:\t\t" + str(self.start) + "\nFile Duration:\t\t" + str(self.duration) + "\nFile End:\t\t" + str(self.end)
+        return "Filename:\t\t" + self.filename + "\nFile start:\t\t" + str(self.start) + "\nFile Duration:\t\t" + str(self.duration) + "\nFile End:\t\t" + str(self.end)
